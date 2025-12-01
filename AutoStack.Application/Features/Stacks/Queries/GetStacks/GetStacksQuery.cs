260c1a13ad261e17f54b89a1c423ec38773d9d0b
Rasmus Høllund Kristensen <31127327+MyMonsterVR@users.noreply.github.com>
2025-12-01 19:00:38 +0100

    Revert "Fixed GetStacks having wrong query names"

--- AutoStack.Application/Features/Stacks/Queries/GetStacks/GetStacksQuery.cs
+++ AutoStack.Application/Features/Stacks/Queries/GetStacks/GetStacksQuery.cs
@@ -7,14 +7,11 @@ namespace AutoStack.Application.Features.Stacks.Queries.GetStacks;
 /// <summary>
 /// Gets multiple stacks
 /// </summary>
-/// <param name="StackSortBy">Property to sort by</param>
-/// <param name="SortingOrder">The order for sorting</param>
+/// <param name="StackSortByResponse">Property to sort by</param>
 /// <param name="StackType">Stack type; null if all stacks should be shown</param>
-/// <param name="PageNumber">The page we are on</param>
-/// <param name="PageSize">How many elements to fetch on one page</param>
 public record GetStacksQuery(
-    StackSortByResponse StackSortBy = StackSortByResponse.Popularity,
-    SortingOrderResponse SortingOrder = SortingOrderResponse.Ascending,
+    StackSortByResponse StackSortByResponse = StackSortByResponse.Popularity,
+    SortingOrderResponse SortingOrderResponse = SortingOrderResponse.Descending,
     StackTypeResponse? StackType = null,
     int PageNumber = 1,
     int PageSize = 20) : IQuery<PagedResponse<StackResponse>>;
