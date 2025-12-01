f83ba3208abd03f38add62f084add335f77b5868
Rasmus Høllund Kristensen <MyMonsterDK@gmail.com>
2025-12-01 19:10:40 +0100

    Fixed PR mistake from doing things a little too fast

--- AutoStack.Application/Features/Stacks/Queries/GetStacks/GetStacksQuery.cs
+++ AutoStack.Application/Features/Stacks/Queries/GetStacks/GetStacksQuery.cs
@@ -7,11 +7,14 @@ namespace AutoStack.Application.Features.Stacks.Queries.GetStacks;
 /// <summary>
 /// Gets multiple stacks
 /// </summary>
-/// <param name="StackSortByResponse">Property to sort by</param>
+/// <param name="StackSortBy">Property to sort by</param>
+/// <param name="SortingOrder">The order for sorting</param>
 /// <param name="StackType">Stack type; null if all stacks should be shown</param>
+/// <param name="PageNumber">The page we are on</param>
+/// <param name="PageSize">How many elements to fetch on one page</param>
 public record GetStacksQuery(
-    StackSortByResponse StackSortByResponse = StackSortByResponse.Popularity,
-    SortingOrderResponse SortingOrderResponse = SortingOrderResponse.Descending,
+    StackSortByResponse StackSortBy = StackSortByResponse.Popularity,
+    SortingOrderResponse SortingOrder = SortingOrderResponse.Ascending,
     StackTypeResponse? StackType = null,
     int PageNumber = 1,
     int PageSize = 20) : IQuery<PagedResponse<StackResponse>>;
